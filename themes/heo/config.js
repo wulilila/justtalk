767db57e8cfd23b898a7e5e248a10319ff966947
wulilila <connoisseurmgg@gmail.com>
2024-10-17 20:28:59 +0800

    Update config.js
      // 英雄区(首页顶部大卡)

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '热爱生活',
+  HEO_HERO_TITLE_2: '共同进步',
+  HEO_HERO_TITLE_3: 'justtalk-b2',
+  HEO_HERO_TITLE_4: '出版发布',
+  HEO_HERO_TITLE_5: '欢迎你一起让网站变的更好',
+  HEO_HERO_TITLE_LINK: '',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
